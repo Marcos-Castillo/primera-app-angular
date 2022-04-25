9a79ebf03b0f664fa898dd0f67a62a2502a9eb3f
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2022-04-24 23:03:52 -0300

    creación de componentes
    creación de componentes header y footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyB3Pwz_bDS6NUQ1r_6A9gEKOx1isxG-Z8U",
+    authDomain: "control-clientes-4d32d.firebaseapp.com",
+    projectId: "control-clientes-4d32d",
+    storageBucket: "control-clientes-4d32d.appspot.com",
+    messagingSenderId: "765627446518",
+    appId: "1:765627446518:web:d4bdb00749a7f5844be8d7"
+  }
 };
 
 /*
